926b9e2142f1d5d1baad2fc3e26c2e64ad6aae74
Niclas <niclas.kroon@protonmail.ch>
2018-09-22 17:29:47 +0200

    Squashed commit of the following:
    commit 284efb83286902370c667879004592581e70f225
    Author: Jan <stoltmanjan@gmail.com>
    Date:   Sat Sep 22 17:06:18 2018 +0200
    
        Ktlint fixes
    
    commit 205b50c5f5be0ce0be19eff5973924468afb9deb
    Merge: 8696bb1 d9ce60a
    Author: Jan Stoltman <stoltmanjan@gmail.com>
    Date:   Sat Sep 22 13:43:36 2018 +0200
    
        Merge branch 'dev' into feature-weather-use-case-tests
    
    commit 8696bb185fbbf551612dcfce717956757ee07276
    Author: Jan <stoltmanjan@gmail.com>
    Date:   Fri Sep 21 18:05:54 2018 +0200
    
        WeatherUseCase tests
    
    commit 5512aa976f4a8d9f36664ed7129424bbbf969422
    Author: Jan <stoltmanjan@gmail.com>
    Date:   Fri Sep 21 16:58:58 2018 +0200
    
        WeatherViewModel tests. MockMaker inline rule added in order to avoid opening classes for mocking.

--- app/src/test/java/fi/kroon/vadret/domain/WeatherUseCaseTest.kt
+++ app/src/test/java/fi/kroon/vadret/domain/WeatherUseCaseTest.kt
@@ -1,3 +1,85 @@
 package fi.kroon.vadret.domain
 
-class WeatherUseCaseTest
+import fi.kroon.vadret.data.Request
+import fi.kroon.vadret.data.exception.Either
+import fi.kroon.vadret.data.exception.Failure
+import fi.kroon.vadret.data.weather.WeatherRepository
+import fi.kroon.vadret.data.weather.model.Weather
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherUseCaseTest {
+    @Mock
+    lateinit var mockWeatherRepository: WeatherRepository
+
+    @Mock
+    lateinit var mockRequest: Request
+
+    @Mock
+    lateinit var mockWeather: Weather
+
+    private lateinit var testUseCase: WeatherUseCase
+
+    private val testThrowable = Throwable()
+
+    @Before
+    fun setup() {
+        testUseCase = WeatherUseCase(mockWeatherRepository)
+    }
+
+    @Test
+    fun repositoryReturnsSingle_shouldPassResult() {
+        val testWeatherEither = createTestWeatherEither(mockWeather)
+        val testSingle = createWeatherSingle(testWeatherEither)
+        Mockito.doReturn(testSingle).`when`(mockWeatherRepository).get(mockRequest)
+
+        testUseCase
+            .get(mockRequest)
+            .test()
+            .assertResult(testWeatherEither)
+    }
+
+    @Test
+    fun repositoryReturnsFailure_shouldPassFailure() {
+        val testFailureEither = createFailureEither()
+        val testSingle = createFailureSingle(testFailureEither)
+        Mockito.doReturn(testSingle).`when`(mockWeatherRepository).get(mockRequest)
+
+        testUseCase
+            .get(mockRequest)
+            .test()
+            .assertResult(testFailureEither)
+    }
+
+    @Test
+    fun repositoryThrowsException_shouldReturnIOExceptionEither() {
+        val testSingle = createThrowableSingle()
+        Mockito.doReturn(testSingle).`when`(mockWeatherRepository).get(mockRequest)
+
+        testUseCase
+            .get(mockRequest)
+            .test()
+            .assertError(Throwable::class.java)
+    }
+
+    private fun createFailureEither() =
+        Either.Left(Failure.NetworkOfflineFailure())
+
+    private fun createFailureSingle(failureEither: Either.Left<Failure>) =
+        Single.just(failureEither)
+
+    private fun createThrowableSingle() =
+        Single.error<Either<Failure, Weather>>(testThrowable)
+
+    private fun createTestWeatherEither(mockWeather: Weather) =
+        Either.Right(mockWeather) as Either<Failure, Weather>
+
+    private fun createWeatherSingle(value: Either<Failure, Weather>) =
+        Single.just(value)
+}
