1c5d056debd6be3b66b1ff49e409d98347986a3c
Niclas <niclas.kroon@protonmail.ch>
2018-09-22 13:03:18 +0200

    Squashed commit of the following:
    commit 386e39c58e2d736a7b4d1b36c415eee8ab27d1c9
    Author: Jan <stoltmanjan@gmail.com>
    Date:   Fri Sep 21 18:12:44 2018 +0200
    
        Tests added for Travis
    
    commit 5512aa976f4a8d9f36664ed7129424bbbf969422
    Author: Jan <stoltmanjan@gmail.com>
    Date:   Fri Sep 21 16:58:58 2018 +0200
    
        WeatherViewModel tests. MockMaker inline rule added in order to avoid opening classes for mocking.

--- app/src/main/java/fi/kroon/vadret/presentation/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/fi/kroon/vadret/presentation/viewmodel/WeatherViewModel.kt
@@ -10,13 +10,10 @@ import io.reactivex.Single
 import timber.log.Timber
 import javax.inject.Inject
 
-const val TAG = "WeatherViewModel"
-
 @VadretApplicationScope
 class WeatherViewModel @Inject constructor(
     private val weatherUseCase: WeatherUseCase
 ) : BaseViewModel() {
-
     fun get(request: Request): Single<Either<Failure, Weather>> = weatherUseCase
         .get(request)
         .doOnEvent { t1, t2 ->
--- app/src/test/java/fi/kroon/vadret/presentation/WeatherViewModelTest.kt
+++ app/src/test/java/fi/kroon/vadret/presentation/WeatherViewModelTest.kt
@@ -1,3 +1,88 @@
 package fi.kroon.vadret.presentation
 
-class WeatherViewModelTest
+import fi.kroon.vadret.data.Request
+import fi.kroon.vadret.data.exception.Either
+import fi.kroon.vadret.data.exception.Failure
+import fi.kroon.vadret.data.weather.model.Weather
+import fi.kroon.vadret.domain.WeatherUseCase
+import fi.kroon.vadret.presentation.viewmodel.WeatherViewModel
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherViewModelTest {
+    @Mock
+    lateinit var mockWeatherUseCase: WeatherUseCase
+
+    @Mock
+    lateinit var mockRequest: Request
+
+    @Mock
+    lateinit var mockWeather: Weather
+
+    private lateinit var testViewModel: WeatherViewModel
+
+    private val testThrowable = Throwable()
+
+    @Before
+    fun setup() {
+        testViewModel = WeatherViewModel(mockWeatherUseCase)
+    }
+
+    @Test
+    fun useCaseReturnsSingle_shouldPassResult() {
+        val testWeatherEither = createTestWeatherEither(mockWeather)
+        val testSingle = createMockWeatherSingle(testWeatherEither)
+        doReturn(testSingle).`when`(mockWeatherUseCase).get(mockRequest)
+
+        testViewModel
+            .get(mockRequest)
+            .test()
+            .assertResult(testWeatherEither)
+    }
+
+    @Test
+    fun useCaseReturnsFailure_shouldPassFailure() {
+        val testFailureEither = createTestFailureEither()
+        val testSingle = createFailureSingle(testFailureEither)
+        doReturn(testSingle).`when`(mockWeatherUseCase).get(mockRequest)
+
+        testViewModel
+            .get(mockRequest)
+            .test()
+            .assertResult(testFailureEither)
+    }
+
+    @Test
+    fun useCaseThrowsException_shouldReturnIOExceptionEither() {
+        val testSingle = createThrowableSingle()
+        doReturn(testSingle).`when`(mockWeatherUseCase).get(mockRequest)
+
+        testViewModel
+            .get(mockRequest)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValueAt(0) { it is Either.Left<Failure> && it.a is Failure.IOException }
+    }
+
+    private fun createTestFailureEither() =
+        Either.Left(Failure.IOException())
+
+    private fun createFailureSingle(failureEither: Either.Left<Failure.IOException>) =
+        Single.just(failureEither)
+
+    private fun createThrowableSingle() =
+        Single.error<Either<Failure, Weather>>(testThrowable)
+
+    private fun createTestWeatherEither(mockWeather: Weather) =
+        Either.Right(mockWeather) as Either<Failure, Weather>
+
+    private fun createMockWeatherSingle(value: Either<Failure, Weather>) =
+        Single.just(value)
+}
