2880955d6709d58a7f2b57c9b8eceee0746443ae
Niclas <niclas.kroon@protonmail.ch>
2018-11-26 13:27:54 +0100

    add missing annotation target

--- app/src/main/java/fi/kroon/vadret/data/alert/adapter/SingleToArray.kt
+++ app/src/main/java/fi/kroon/vadret/data/alert/adapter/SingleToArray.kt
@@ -2,6 +2,7 @@ package fi.kroon.vadret.data.alert.adapter
 
 import com.squareup.moshi.JsonQualifier
 import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.FIELD
 
 /**
  *  Annotate JSON fields
@@ -9,6 +10,7 @@ import kotlin.annotation.AnnotationRetention.RUNTIME
  *  is accessible directly
  *  rather than in a List::class
  */
+@Target(FIELD)
 @Retention(RUNTIME)
 @JsonQualifier
 annotation class SingleToArray
