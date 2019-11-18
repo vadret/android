1d4612843c5f66fd84b0764ac58dec0df5048ae0
Niclas <niclas.kroon@protonmail.ch>
2019-11-18 18:42:52 +0100

    fixes #185 -- Cannot serialize Kotlin type Address

--- app/src/main/java/fi/kroon/vadret/data/nominatim/model/Address.kt
+++ app/src/main/java/fi/kroon/vadret/data/nominatim/model/Address.kt
@@ -2,25 +2,15 @@ package fi.kroon.vadret.data.nominatim.model
 
 import android.os.Parcelable
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@JsonClass(generateAdapter = true)
 data class Address(
-
-    @Json(name = "building")
-    val building: String,
-
-    @Json(name = "house_number")
-    val houseNumber: String,
 
     @Json(name = "road")
     val road: String,
-
-    @Json(name = "neighbourhood")
-    val neighbourhood: String,
-
-    @Json(name = "suburb")
-    val suburb: String,
 
     @Json(name = "village")
     val village: String?,
@@ -31,17 +21,11 @@ data class Address(
     @Json(name = "hamlet")
     val hamlet: String?,
 
-    @Json(name = "city_district")
-    val cityDistrict: String,
-
     @Json(name = "city")
     val city: String?,
 
     @Json(name = "state")
     val state: String?,
-
-    @Json(name = "post_code")
-    val postCode: String,
 
     @Json(name = "country")
     val country: String,
